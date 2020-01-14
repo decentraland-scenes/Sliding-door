6e64676f19d319a167b375e8fded99961e48982d
nearnshaw <nearnshaw@decentraland.org>
2020-01-14 14:26:43 -0300

    hover feedback

--- src/game.ts
+++ src/game.ts
@@ -1,37 +1,44 @@
-import utils from "../node_modules/decentraland-ecs-utils/index"
-
+import utils from '../node_modules/decentraland-ecs-utils/index'
 
 // Define fixed walls
 const wall1 = new Entity()
-wall1.addComponent(new Transform({
-  position: new Vector3(5.75, 1, 3),
-  scale: new Vector3(1.5, 2, 0.1)
-}))
+wall1.addComponent(
+  new Transform({
+    position: new Vector3(5.75, 1, 3),
+    scale: new Vector3(1.5, 2, 0.1)
+  })
+)
 wall1.addComponent(new BoxShape())
 engine.addEntity(wall1)
 
 const wall2 = new Entity()
-wall2.addComponent(new Transform({
-  position: new Vector3(2.25, 1, 3),
-  scale: new Vector3(1.5, 2, 0.1)
-}))
+wall2.addComponent(
+  new Transform({
+    position: new Vector3(2.25, 1, 3),
+    scale: new Vector3(1.5, 2, 0.1)
+  })
+)
 wall2.addComponent(new BoxShape())
 engine.addEntity(wall2)
 
 // Add the two sides to the door
 const doorL = new Entity()
-doorL.addComponent(new Transform({
-  position: new Vector3(0.5, 0, 0),
-  scale: new Vector3(1.1, 2, 0.05)
-}))
+doorL.addComponent(
+  new Transform({
+    position: new Vector3(0.5, 0, 0),
+    scale: new Vector3(1.1, 2, 0.05)
+  })
+)
 doorL.addComponent(new BoxShape())
 engine.addEntity(doorL)
 
 const doorR = new Entity()
-doorR.addComponent(new Transform({
-  position: new Vector3(-0.5, 0, 0),
-  scale: new Vector3(1.1, 2, 0.05)
-}))
+doorR.addComponent(
+  new Transform({
+    position: new Vector3(-0.5, 0, 0),
+    scale: new Vector3(1.1, 2, 0.05)
+  })
+)
 doorR.addComponent(new BoxShape())
 engine.addEntity(doorR)
 
@@ -46,49 +53,39 @@ doorL.addComponent(doorMaterial)
 doorR.addComponent(doorMaterial)
 
 // Define open and closed positions for both doors
-let doorLClosed =  new Vector3(0.5, 0, 0)
+let doorLClosed = new Vector3(0.5, 0, 0)
 let doorLOpen = new Vector3(1.25, 0, 0)
-let doorRClosed =  new Vector3(-0.5, 0, 0)
+let doorRClosed = new Vector3(-0.5, 0, 0)
 let doorROpen = new Vector3(-1.25, 0, 0)
-
 
 // This parent entity holds the state for both door sides
 const doorParent = new Entity()
-doorParent.addComponent(new Transform({
-  position: new Vector3(4, 1, 3)
-}))
+doorParent.addComponent(
+  new Transform({
+    position: new Vector3(4, 1, 3)
+  })
+)
 
 //toggle behavior for door
-doorParent.addComponent(new utils.ToggleComponent(utils.ToggleState.Off, value =>{
-	if (value == utils.ToggleState.On){
-		doorL.addComponentOrReplace(
-			new utils.MoveTransformComponent(
-				doorLClosed,
-				doorLOpen,
-				1
-			))
-		doorR.addComponentOrReplace(
-				new utils.MoveTransformComponent(
-					doorRClosed,
-					doorROpen,
-					1
-				))
-	}
-	else{
-		doorL.addComponentOrReplace(
-			new utils.MoveTransformComponent(
-				doorLOpen,
-				doorLClosed,
-				1
-			))
-		doorR.addComponentOrReplace(
-				new utils.MoveTransformComponent(
-					doorROpen,
-					doorRClosed,
-					1
-				))
-	}
-}))
+doorParent.addComponent(
+  new utils.ToggleComponent(utils.ToggleState.Off, value => {
+    if (value == utils.ToggleState.On) {
+      doorL.addComponentOrReplace(
+        new utils.MoveTransformComponent(doorLClosed, doorLOpen, 1)
+      )
+      doorR.addComponentOrReplace(
+        new utils.MoveTransformComponent(doorRClosed, doorROpen, 1)
+      )
+    } else {
+      doorL.addComponentOrReplace(
+        new utils.MoveTransformComponent(doorLOpen, doorLClosed, 1)
+      )
+      doorR.addComponentOrReplace(
+        new utils.MoveTransformComponent(doorROpen, doorRClosed, 1)
+      )
+    }
+  })
+)
 
 engine.addEntity(doorParent)
 
@@ -96,16 +93,21 @@ engine.addEntity(doorParent)
 doorL.setParent(doorParent)
 doorR.setParent(doorParent)
 
-
 // Set the click behavior for the door
 doorL.addComponent(
-  new OnClick(e => {
-    doorParent.getComponent(utils.ToggleComponent).toggle()
-  })
+  new OnPointerDown(
+    e => {
+      doorParent.getComponent(utils.ToggleComponent).toggle()
+    },
+    { button: ActionButton.POINTER, hoverText: 'Open/Close' }
+  )
 )
 
 doorR.addComponent(
-  new OnClick(e => {
-    doorParent.getComponent(utils.ToggleComponent).toggle()
-  })
+  new OnPointerDown(
+    e => {
+      doorParent.getComponent(utils.ToggleComponent).toggle()
+    },
+    { button: ActionButton.POINTER, hoverText: 'Open/Close' }
+  )
 )
